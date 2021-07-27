b00cee633f7fda2f54486628a191e2854524227d
RitvickRaghuvanshi <86033847+RitvickRaghuvanshi@users.noreply.github.com>
2021-07-26 21:31:41 -0700

    Add files via upload

---
+++ Tower-Siege-1-Template-main/Polygon.js
@@ -0,0 +1,24 @@
+class Polygon{
+    constructor(x, y, width, height) {
+        var options = {
+                     
+        }
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.width = width;
+        this.height = height;
+        World.add(world, this.body);
+        
+        this.polygon = loadImage("polygon.png");
+      }
+      display(){
+        image(this.polygon,0,0, this.width, this.height);
+        var angle = this.body.angle;
+        var pos= this.body.position;
+        push();
+        translate(pos.x, pos.y);
+        rotate(angle);
+        rectMode(CENTER);
+        rect(0,0,this.width, this.height);
+        pop();
+      }
+}
